d76aeafaf06e83b2da054bc85f1d25c28930afac
Serikarakhil1209 <serikarakhil5@gmail.com>
2025-07-28 11:56:10 +0530

    cloud connection is added mongo atlas

--- Config/database.js
+++ Config/database.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose")
 
+url = "mongodb+srv://serikarakhil5:yOvAwausiS7FXLyp@newtodo.r1k3nrx.mongodb.net/"
+// url ="mongodb://localhost:27017"
+
  async function create_data_base(){
     try {
-        await mongoose.connect("mongodb://localhost:27017",{
+        await mongoose.connect(url,{
             dbName: "mydb"
         });
         console.log("db is created")
